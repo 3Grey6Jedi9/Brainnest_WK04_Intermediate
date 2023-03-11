7a5fff33a4c1cd9b667adca157085f9b66651b53
Darth Vader <danieltarancon@gmail.com>
2023-03-11 21:49:29 +0100

    Update

--- b.py
+++ b.py
@@ -1,9 +1,4 @@
-coins = [2, 1, 0.5, 0.2 ,0.1, 0.05, 0.02, 0.01]
+import itertools
 
-
-
-for c in coins:
-    print(c)
-    for d in coins[coins.index(c)+1:]:
-        print(d)
-        break
+for a in itertools.product(range(1,10), repeat=2):
+    print(a)
